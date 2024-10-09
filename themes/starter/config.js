6bbf23d328454988d5bf4313d78b8d4d59e18473
zaopao <59226005+zaopao@users.noreply.github.com>
2024-10-09 08:40:52 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -59,16 +59,16 @@ const CONFIG = {
 
   // 首页ABOUT区块
   STARTER_ABOUT_ENABLE: true, // ABOUT区块开关
-  STARTER_ABOUT_TITLE: '一套轻量实用的建站解决方案',
+  STARTER_ABOUT_TITLE: '一套轻量实用的馄饨摊解决方案',
   STARTER_ABOUT_TEXT:
-    'NotionNext的愿景是帮助非技术人员的小白，最低成本、最快速地搭建自己的网站，帮助您将自己的产品与故事高效地传达给世界。 <br /> <br /> 功能强大的Notion笔记，简单快速的Vercel托管平台，组成一个简单的网站',
+    '馄饨摊的愿景是帮助初入摆摊事业的摊主，最低成本、最快速地搭建自己的生意，帮助您将自己的产品与故事高效地传达给世界。 <br /> <br /> 强大的馄饨摊经营方案，广受好评的馄饨配方',
   STARTER_ABOUT_BUTTON_TEXT: '了解更多',
-  STARTER_ABOUT_BUTTON_URL: 'https://docs.tangly1024.com/about',
+  STARTER_ABOUT_BUTTON_URL: 'https://99.sc.cn/n9v54q3dv418/pc/goods-204357.html',
   STARTER_ABOUT_IMAGE_1: '/images/starter/about/about-image-01.jpg',
   STARTER_ABOUT_IMAGE_2: '/images/starter/about/about-image-02.jpg',
-  STARTER_ABOUT_TIPS_1: '7000+',
-  STARTER_ABOUT_TIPS_2: '博客站点',
-  STARTER_ABOUT_TIPS_3: '正在线上运行',
+  STARTER_ABOUT_TIPS_1: '9000+',
+  STARTER_ABOUT_TIPS_2: '馄饨摊主',
+  STARTER_ABOUT_TIPS_3: '正在经营生意',
 
   // 首页价格区块
   STARTER_PRICING_ENABLE: true, // 价格区块开关
